e3920b50f58a3b5b298afa5db3166591e8ac9594
Antonio Carvajal <mercutos@gmail.com>
2022-09-25 23:50:16 +0200

    ADD properties file

---
+++ configuration/properties.py
@@ -0,0 +1,6 @@
+ORGANIZATION = "<YOUR_ORG_CODE>"
+API_KEY = "<YOUR_API_TOKEN>"
+TITLE = "Destino"
+GENRE = "Romance"
+CHAPTERS = 3
+SUMMARY = "Juan no creia en el destino hastaque conoció a María y sus vidas cambiaron por completo."
